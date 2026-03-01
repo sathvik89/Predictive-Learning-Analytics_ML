0aa1236c9d668d636d104c9502f993963ce540b6
Apoorv Choudhary <apoorvachoudhary506@gmail.com>
2026-03-01 14:43:45 +0530

    ui changed

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import joblib
-import plotly.graph_objects as go
 import plotly.express as px
+import plotly.graph_objects as go
+import joblib
+
 
 # ============================================================
 # PAGE CONFIG
